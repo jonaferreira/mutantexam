313ff3b42299d48bd8f39e425a4df69f72477299
FERREIRA <B040650@servexternos.santandertecnologia.com.ar>
2019-09-16 01:56:15 -0300

    Add files of configuration and build

---
+++ test/run.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+cd $(dirname $0)
+
+cd ../complete
+
+./mvnw clean package
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+./mvnw clean install dockerfile:build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
+./gradlew build docker
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
+cd ../initial
+
+./mvnw clean compile
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+./gradlew compileJava
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
+exit
